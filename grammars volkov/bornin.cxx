e503da0b420ad5a9fb57ef22ffca33e2995ef627
volkovalexeytsu <57768173+volkovalexeytsu@users.noreply.github.com>
2020-02-04 16:18:04 +0500

    Update bornin.cxx

--- grammars volkov/bornin.cxx
+++ grammars volkov/bornin.cxx
@@ -5,7 +5,7 @@ Strem -> "стремящемс";
 Function -> Noun<kwtype=function> Variable "конец";
 Num -> AnyWord<wff=/[1-9]?[0-9]{1,6}/>;
 
-S -> "предел" Function interp(Predel.Function) "при" Variable interp(Predel.Variable) Strem "к" Num interp(Predel.To) "конец" "предел";
+S -> "предел" Function interp(Predel.Function) "при" Variable interp(Predel.Variable) Strem "к" Num interp(Predel.To) "конец" "пред";
 
 L -> (S interp(Fraction.Chislitel))(Num interp(Fraction.Chislitel)) "на" (S interp(Fraction.Znamenatel))(Num interp(Fraction.Znamenatel));
 
