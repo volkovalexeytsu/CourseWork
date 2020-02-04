1a06ab1ee8eec15a78c3aa9168b3cef1dcb70f67
volkovalexeytsu <57768173+volkovalexeytsu@users.noreply.github.com>
2020-02-04 14:41:42 +0500

    Add files via upload

---
+++ grammars volkov/bornin.cxx
@@ -0,0 +1,11 @@
+#encoding "utf-8"
+#GRAMMAR_ROOT L
+Variable -> Noun<kwtype=variable>;
+Strem -> "стремящемс";
+Function -> Noun<kwtype=function> Variable "конец";
+Num -> AnyWord<wff=/[1-9]?[0-9]{1,6}/>;
+
+S -> "предел" Function interp(Predel.Function) "при" Variable interp(Predel.Variable) Strem "к" Num interp(Predel.To);
+
+L -> (S interp(Fraction.Chislitel))(Num interp(Fraction.Chislitel)) "на" (S interp(Fraction.Znamenatel))(Num interp(Fraction.Znamenatel));
+
