65cdfe71f361b07de1512e6767b65fd384073947
volkovalexeytsu <57768173+volkovalexeytsu@users.noreply.github.com>
2020-03-01 16:42:10 +0500

    Add files via upload

--- grammars volkov/bornin.cxx
+++ grammars volkov/bornin.cxx
@@ -1,11 +1,14 @@
 #encoding "utf-8"
-#GRAMMAR_ROOT L
-Variable -> Noun<kwtype=variable>;
-Strem -> "стремящемс";
-Function -> Noun<kwtype=function> Variable "конец" "функ";
+#GRAMMAR_ROOT M 
+//Variable -> Noun<kwtype=variable>;
+//Strem -> "стремящемс";
+//Function -> Noun<kwtype=function> Variable "конец" "функ";
 Num -> AnyWord<wff=/[1-9]?[0-9]{1,6}/>;
+Exp -> 'корень' 'из'  Exp  interp (Sqrt.Recursion) 'конец' 'подкорня';
+Exp -> Num interp(Sqrt.Value);
 
-S -> "предел" Function interp(Predel.Function) "при" Variable interp(Predel.Variable) Strem "к" Num interp(Predel.To) "конец" "пред";
+//S -> "предел" Function interp(Predel.Function) "при" Variable interp(Predel.Variable) Strem "к" Num interp(Predel.To);
 
-L -> (S interp(Fraction.Chislitel))(Num interp(Fraction.Chislitel)) "на" (S interp(Fraction.Znamenatel))(Num interp(Fraction.Znamenatel));
+//L -> (S interp(Fraction.Chislitel))(Num interp(Fraction.Chislitel)) "на" (S interp(Fraction.Znamenatel))(Num interp(Fraction.Znamenatel));
 
+M -> (Exp)".";
